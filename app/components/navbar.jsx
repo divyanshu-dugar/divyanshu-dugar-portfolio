8f8745425a08cdd39b80056b5666070bf0ad2305
Divyanshu Dugar <divyu2004@gmail.com>
2025-05-27 20:04:23 -0400

    increase font size

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,3 +1,4 @@
+
 export default function Navbar() {
     return (
         <div className="navbar bg-base-100 shadow-sm">
@@ -8,21 +9,25 @@ export default function Navbar() {
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-bold">
                         <li><a>Home</a></li>
                         <li><a>About</a></li>
+                        <li><a>Skills</a></li>
                         <li><a>Projects</a></li>
-                        <li><a>My Library</a></li>
+                        <li><a>Work Experience</a></li>
+                        <li><a href="/notes">My Library</a></li>
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">DD</a>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li><a>About</a></li>
-                    <li><a>Projects</a></li>
-                    <li><a>My Library</a></li>
+                <ul className="menu menu-horizontal px-1 font-bold font-size-l text-lg">
+                        <li><a href="/">Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Skills</a></li>
+                        <li><a>Projects</a></li>
+                        <li><a>Work Experience</a></li>
+                        <li><a href="/notes">My Library</a></li>
                 </ul>
             </div>
             <div className="navbar-end">
