e7f007a929e8e6f44de81b339998fef9dbc48c6a
Divyanshu Dugar <divyu2004@gmail.com>
2025-05-27 20:04:38 -0400

    added Skills Toolbox to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/app/components/navbar";
 import HomeHero from "@/app/components/homeHero";
+import SkillsToolbox from "./components/skillsToolBox";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HomeHero />
+      <SkillsToolbox/>
     </>
   );
 }
