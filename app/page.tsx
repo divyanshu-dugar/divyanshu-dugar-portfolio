c3d293b063e86c8f260f0e9fd89fc20de13be30a
Divyanshu Dugar <divyu2004@gmail.com>
2025-06-02 17:30:47 -0400

    fixed imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/app/components/navbar";
 import HomeHero from "@/app/components/homeHero";
-import SkillsToolbox from "./components/skillsToolBox";
+import SkillsToolbox from "@/app/components/skillsToolbox";
 
 export default function Home() {
   return (
